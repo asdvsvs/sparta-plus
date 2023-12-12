e45d1c23aa31a758e9651508a617ab2cdc557616
asdvsvs <gag973000@gmail.com>
2023-12-12 22:30:47 +0900

    Feat : 멤버 엔티티 추가

---
+++ src/main/java/com/sparta/plus/entity/Member.java
@@ -0,0 +1,32 @@
+package com.sparta.plus.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(nullable = false, unique = true)
+    private String memberName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
