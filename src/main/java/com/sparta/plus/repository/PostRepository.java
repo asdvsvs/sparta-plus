8c7dd80d6dd6a08a9aae7866b6f1b748adaa3cde
asdvsvs <gag973000@gmail.com>
2023-12-18 19:47:54 +0900

    Feat : 게시글의 멤버를 지연로딩 설정해서 쿼리도 페치조인 설정

--- src/main/java/com/sparta/plus/repository/PostRepository.java
+++ src/main/java/com/sparta/plus/repository/PostRepository.java
@@ -1,8 +1,14 @@
 package com.sparta.plus.repository;
 
 import com.sparta.plus.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    @Override
+    @Query("select p from Post p left join fetch p.member")
+    Page<Post> findAll(Pageable pageable);
 }
