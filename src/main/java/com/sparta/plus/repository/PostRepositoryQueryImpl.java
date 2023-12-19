54a989913db8a933286bf97250b815ba4ef334b8
asdvsvs <gag973000@gmail.com>
2023-12-19 19:13:36 +0900

    Refactor : QueryDSL 조건 메서드화

--- src/main/java/com/sparta/plus/repository/PostRepositoryQueryImpl.java
+++ src/main/java/com/sparta/plus/repository/PostRepositoryQueryImpl.java
@@ -2,6 +2,7 @@ package com.sparta.plus.repository;
 
 import static com.sparta.plus.entity.QPost.post;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sparta.plus.entity.Post;
@@ -15,13 +16,22 @@ public class PostRepositoryQueryImpl implements PostRepositoryQuery {
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public List<Post> searchByContainsTitleAndMember(String title, String memberName) {
+    public List<Post> searchByContainsTitleAndMember(String titleCondition,
+        String memberNameCondition) {
         JPAQuery<Post> query = jpaQueryFactory.select(post)
             .from(post)
             .leftJoin(post.member).fetchJoin()
-            .where(post.title.contains(title)
-                .and(post.member.memberName.eq(memberName))
+            .where(titleContains(titleCondition),
+                memberNameEq(memberNameCondition)
             );
         return query.fetch();
+    }
+
+    private BooleanExpression titleContains(String titleCondition) {
+        return titleCondition != null ? post.title.contains(titleCondition) : null;
+    }
+
+    private BooleanExpression memberNameEq(String memberNameCondition) {
+        return memberNameCondition != null ? post.member.memberName.eq(memberNameCondition) : null;
     }
 }
