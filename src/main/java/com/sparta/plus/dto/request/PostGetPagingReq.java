24e51304959d88cbbb5d8453b60d898d62cab9bc
asdvsvs <gag973000@gmail.com>
2023-12-22 03:01:21 +0900

    Feat : dto에 빌더 추가

--- src/main/java/com/sparta/plus/dto/request/PostGetPagingReq.java
+++ src/main/java/com/sparta/plus/dto/request/PostGetPagingReq.java
@@ -1,8 +1,10 @@
 package com.sparta.plus.dto.request;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class PostGetPagingReq {
 
     private int page;
--- src/main/java/com/sparta/plus/dto/request/PostSaveReq.java
+++ src/main/java/com/sparta/plus/dto/request/PostSaveReq.java
@@ -2,9 +2,15 @@ package com.sparta.plus.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PostSaveReq {
 
     @NotBlank
